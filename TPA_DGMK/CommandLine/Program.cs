d7d856a789255edc151bf7c7a264d360207fc767
DejF <gi3r4s@gmail.com>
2018-11-07 06:43:01 +0100

    Some changes are made, still there's problem with nullpointer exception - needs work

--- TPA_DGMK/CommandLine/Program.cs
+++ TPA_DGMK/CommandLine/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace CommandLine
+﻿using CommandLine;
+using Logging;
+
+namespace CommandLineInterface
 {
     class Program
     {
+        private static Logger logger;
         static void Main(string[] args)
         {
+            logger = new FileLogger();
+            CLView view = new CLView(logger);
+            logger.Write(SeverityEnum.Information, "The program has been started");
+            view.Run();
+            logger.Write(SeverityEnum.Information, "The program has been ended");
         }
     }
 }
